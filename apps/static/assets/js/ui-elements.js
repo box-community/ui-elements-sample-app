a06b899cabefb8651eb14da77d2197610469f76f
Barduino <barduinor@gmail.com>
2022-09-06 16:17:10 -0400

    refactor ui elements; ajax refresh;

--- apps/static/assets/js/ui-elements.js
+++ apps/static/assets/js/ui-elements.js
@@ -23,11 +23,12 @@ function renderPreview_single(accessToken, container, file_id) {
     contentPreviewer.show(file_id, accessToken, options);
 }
 
-function renderUploader_single(accessToken, container, folder_id, documentType, booking_diver_id) {
+function renderUploader_single(accessToken, container, folder_id, documentType, booking_diver_id, callback, url_event, url_refresh, element) {
     var options = {
         "container": container,
         "fileLimit": 1,
     }
+   
 
     // console.log('options:', options);
 
@@ -41,43 +42,70 @@ function renderUploader_single(accessToken, container, folder_id, documentType, 
 
     function uploaderClose(e) {
         console.log('close:', e);
-        // sendEventToServer('close', e);
+        callback('close', e, documentType, booking_diver_id, url_event, element, url_refresh);
     }
 
     function uploaderComplete(e) {
         console.log('complete:', e);
-        sendEventToServer('complete', e);
+        callback('complete', e, documentType, booking_diver_id, url_event, element, url_refresh);
     }
 
     function uploaderUpload(e) {
         console.log('upload:', e);
-        // sendEventToServer('upload', e);
+        callback('upload', e, documentType, booking_diver_id, url_event, element, url_refresh);
     }
 
     function uploaderError(e) {
         console.log('error:', e);
-        // sendEventToServer('error', e);
+        callback('error', e, documentType, booking_diver_id, url_event, element, url_refresh);
     }
 
-    function sendEventToServer(eventType, e) {
-
-        var localData = {
-            eventType: eventType,
-            documentType: documentType,
-            booking_diver_id: booking_diver_id,
-            e: e
-        };
-
-        // TODO Add ajax refresh to the page element
-
-        $.ajax({
-            url: "{{ url_for('booking_blueprint.event') }}",
-            type: 'POST',
-            data: JSON.stringify(localData),
-            contentType: 'application/json;charset=UTF-8',
-        });
-    };
+    
 
     contentUploader.show(folder_id, accessToken, options);
 
 }
+
+function uploaderSendEventToServer(eventType, e, documentType, booking_diver_id, url_event, element, url_refresh) {
+
+    var localData = {
+        eventType: eventType,
+        documentType: documentType,
+        booking_diver_id: booking_diver_id,
+        e: e
+    };
+
+    // TODO Add ajax refresh to the page element
+    if (eventType == 'complete')
+        $.ajax({
+            url: url_event,
+            type: 'POST',
+            data: JSON.stringify(localData),
+            contentType: 'application/json;charset=UTF-8',
+        })
+            .then((data) => {
+                console.log('Posted file upload',data);
+                // location.reload(true);
+                $.ajax({
+                    url: url_refresh,
+                    type: 'GET',
+                })
+                .then((data) => {
+                    location.reload(true)
+                    // var parsed = $.parseHTML(data);
+                    // var divElement = $(parsed).find('#'+element.id)[0];
+                    // $(element).replaceWith(divElement);
+                    // var accessToken = divElement.dataset.token
+                    // var file_id = divElement.dataset.file_id
+                    // var container = divElement.className
+                    // renderPreview_single(accessToken,container , file_id)
+
+                })
+                .catch((error) => {
+                    console.log('Error refreshing booking',error);
+                });
+            })
+            .catch((err) => {
+                console.log('File upload error',err);
+            });
+};
